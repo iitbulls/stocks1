5b233bb63e7038838bcd76c87c443d4d318ae01e
iitbulls <bhansalianuj@gmail.com>
2017-12-29 23:09:12 +0530

    deleted files

--- date.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import os
-for filename in os.listdir('db'):
-    df= pd.read_csv('db/%s'%filename)
-    df['TIMESTAMP'] = pd.to_datetime(df['TIMESTAMP'], infer_datetime_format=True)
-    df.to_csv('db/%s'%filename,index=False)
--- importnse.py
+++
@@ -1,9 +0,0 @@
-from nsepy.history import get_price_list
-import datetime
-import time
-def download_bhav(y,m,d):
-    prices = get_price_list(dt=datetime.date(y,m,d))
-    prices.to_csv('db/test2.csv',index=False)
-    time.sleep(7)
-download_bhav(2017,9,15)
-
